bbc9f214cd5aa41b65e6df31c8ce87412748b049
ubcbent <bentisserand98@gmail.com>
2019-05-30 23:47:13 -0700

    I'm very stupid

---
+++ incrementalBogoVisualization/src/incrementalBogoVisualization/InsertionSort.java
@@ -0,0 +1,53 @@
+package incrementalBogoVisualization;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class InsertionSort extends Sort{
+	
+	private ArrayList<Integer> sorted;
+	private ArrayList<Integer> unsorted;
+	
+	public void initialize(int size) {
+		super.initialize(size);
+		
+		// in insertion sort we also should initialize the sorted and unsorted portions
+		sorted = new ArrayList<Integer>();
+		sorted.add(array.get(0));
+		unsorted = new ArrayList<Integer>();
+		for(int i = 1; i<array.size();i++) {
+			unsorted.add(array.get(i));
+		}
+	}
+	
+	/** currently displays between each full placement (wont show the element moving into place)
+	 * on each given frame, the compared (red) elements will actually be the last place an element was placed
+	 * as well as the next element to be placed.
+	 * This sort of fails to achieve the purpose (showing compared elements) but at a high speed it looks fine.
+	 * (don't tell anyone, shh)
+	 */
+	public boolean sortStep() {
+		// terminate when done
+		if(unsorted.size()==0) {
+			return true;
+		}
+		
+		nummoves++;
+		// perform one step of the sort
+		int val = unsorted.remove(0);
+		int index = sorted.size();
+		comparingIndex[0] = index+1;
+		for(;index>0;index--) {
+			if(val>sorted.get(index-1)) {break;}
+		}
+		comparingIndex[1] = index;
+		sorted.add(index, val);
+		
+		array = new ArrayList<Integer>();
+		array.addAll(sorted);
+		array.addAll(unsorted);
+		
+		return false;
+	}
+	
+}
---
+++ incrementalBogoVisualization/src/incrementalBogoVisualization/Sort.java
@@ -0,0 +1,26 @@
+package incrementalBogoVisualization;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public abstract class Sort {
+	
+	public ArrayList<Integer> array;
+	public double nummoves;
+	public double numcomparisons;
+	public int[] comparingIndex;
+	public int numcorrect;
+	
+	public void initialize(int size) {
+		// only for starting things off, keep even if we change the sort
+		array = new ArrayList<Integer>();
+		comparingIndex = new int[] {-1,-1};
+		numcorrect = 0;
+		nummoves = 0;
+		numcomparisons = 0;
+		for(int i = 0; i<size; i++) {
+			array.add(i+1); //1 to size
+		}
+		Collections.shuffle(array);
+	}
+}
